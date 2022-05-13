b8cc740db3cea0850c907863f0f136db00e8bcac
Lewis Benson <tm.lewis.benson@google.com>
2022-05-13 06:29:20 +0300

    add functional seeder to populate database

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,12 +1,44 @@
+// import the mongoose package to handle the mongoDB
 const mongoose = require('mongoose');
 
+
+// an array of 1000 random cities with locations included to make a fake database of camps
+const cities = require('./cities');
+
+// an array of random words that sound like parts of camp names
+const { places, descriptors } = require('./seedHelpers');
+
+// importing the campground model to be populated by camps
+const CampgroundM = require('../models/campground');
+
+// starting the database
 main().catch(err => console.log(err));
 
 async function main() {
     await mongoose.connect('mongodb://localhost:27017/yelpCamp');
+    console.log('mongoose connected')
 }
 
-const Campground = require('./models/campground')
 
 
+
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await CampgroundM.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new CampgroundM({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
 
